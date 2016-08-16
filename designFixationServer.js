d9110f955f3413181b9e2d506fca5940d2f55d4a
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2016-08-16 02:31:53 -0500

    more cors tweaking

--- designFixationServer.js
+++ designFixationServer.js
@@ -1,6 +1,6 @@
 var app = require('express')()
 var http = require('http').Server(app)
-var io = require('socket.io', {origins: 'chrome-extension:*'})(http)
+var io = require('socket.io')(http, {origins: '*:*'})
 var mongoose = require('mongoose')
 
 var Example = require('./models/example')
